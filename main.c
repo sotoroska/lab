918dbab7c193e0e6d173b3fa86b2e69ea496b5ff
Ann <annarezaeva17@rambler.ru>
2018-12-19 23:57:11 +0300

    Delete file.txt & Add main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "sdt.h"
+
+// This program is just an example one under VCS.
+int main()
+{
+	int a, b;
+
+	cout << "Enter A and B: ";
+
+	cin >> a >> b;
+
+	cout << "A + B = " << a + b << '\n'
+		<< "A - B = " << a - b << '\n'
+		<< "A * B = " << a * b << '\n';
+}
